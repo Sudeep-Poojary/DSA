f2b8bacc723d8208c479e25f830bee8dfceb747b
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-07-01 21:24:10 +0530

    Initial Commit

---
+++ Lecture 10/Arrays/Lecture10_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Lecture10_1 {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 98;
+        marks[1] = 96;
+        marks[2] = 94;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
