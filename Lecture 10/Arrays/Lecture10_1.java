f72c6fdcead85f92aa6054d761847739810aa83a
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-07-03 00:17:07 +0530

    Various ways to declare an array

--- Lecture 10/Arrays/Lecture10_1.java
+++ Lecture 10/Arrays/Lecture10_1.java
@@ -2,14 +2,19 @@ import java.util.*;
 
 public class Lecture10_1 {
     public static void main(String[] args) {
-        int[] marks = new int[3];
 
-        // Another way to declare an array
+        // 1. First way to declare an array
+        // int[] marks = new int[3];
+
+        // 2. Second way to declare an array
         // int marks[] = new int[3];
 
-        marks[0] = 98;
-        marks[1] = 96;
-        marks[2] = 94;
+        // marks[0] = 98;
+        // marks[1] = 96;
+        // marks[2] = 94;
+
+        // 3. Third way to declare an array
+        int marks[] = { 98, 96, 94 };
 
         // System.out.println(marks[0]);
         // System.out.println(marks[1]);
@@ -18,5 +23,6 @@ public class Lecture10_1 {
         for (int i = 0; i < 3; i++) {
             System.out.println(marks[i]);
         }
+
     }
 }
