4b1f02576d2503cbdc54460376621be1294d2bf5
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-08-20 01:33:59 +0530

    Tower of Hanoi

--- Lecture 17/Recursion/02 Medium/Lecture17_8.java
+++ Lecture 17/Recursion/02 Medium/Lecture17_8.java
@@ -1,7 +1,21 @@
 // Tower of Hanoi
+// Time Complexity = O(2^n - 1) == O(2^n)
 
 public class Lecture17_8 {
-    public static void main(String[] args) {
 
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+            return;
+        }
+        towerOfHanoi(n - 1, src, dest, helper);
+        System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+
+        towerOfHanoi(n, "S", "H", "D");
     }
 }
